da1c7d38557d2f57b1ab0ea865c8e04fd27012f5
MOHAMED <mohamedelafandy593@gmail.com>
2023-08-23 12:27:07 -0700

    simple shell

---
+++ shell.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ * @argv: Arguments
+ * @argc: Number of arguments
+ * @env: Envrionment
+ * Return: 0
+ */
+
+int main(int argc, char **argv, char **env)
+{
+	(void)argc;
+	signal(SIGINT, handle_sign_sigint);
+	shell_loop(argv, env);
+	return (0);
+}
